6a52246d189f48981079813bb6d690c56a194380
Ben Shippey <shippeyben@gmail.com>
2023-07-29 16:50:58 -0400

    Create initial logic for BoardColumns class

--- program.py
+++ program.py
@@ -4,19 +4,26 @@ from collections import namedtuple
 
 from pygame.locals import *
 
-# Initialize Pygame
-pygame.init()
-
-# Set up the display
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height))
-
 class Token(pygame.sprite.Sprite):
 	def __init__(self):
 		self.surf = pygame.Surface((75,75))
 		self.surf.fill((255,0,0))
 		self.surf.set_colorkey((255,255,255),RLEACCEL)
 		self.rect = self.surf.get_rect()
+	
+	def find_open_board_cell(self, Board):
+		'''
+		Find and move the token to the available board cell
+		'''
+
+		'''
+		....PPP
+		get the set of board columns
+		identify and select which of the board columns positions is the closest
+		determine whether any of the board column cells are populated
+		determine whether the token is in correct part of the screen for it to be dropped
+		'''
+		pass
 
 class Board():
 	'''
@@ -27,18 +34,6 @@ class Board():
 	dragging and dropping a token, the game runtime should be able to determine which slot
 	the token falls into.
 	'''
-
-	class BoardCell(pygame.sprite.Sprite):
-		def __init__(self, size, position, coordinate):
-			super(Board.BoardCell,self).__init__()
-			self.surf = pygame.Surface((size,size))
-			self.rect = self.surf.get_rect()
-			self.rect = pygame.draw.rect(self.surf, color=(255,0,0), rect=self.rect, width=2)
-			
-			self.coordinate = coordinate
-			self.rect.left = position[0]
-			self.rect.top = position[1]
-
 	def __init__(self, board_size, board_position, cell_width):
 		self.board_size = board_size # enforcing provided type?
 		self.board_position = board_position
@@ -47,15 +42,16 @@ class Board():
 
 		num_columns = board_size[0]
 		num_rows = board_size[1]
-		column_position_list = []
-		row_position_list = []
 
+	
 		# assign column positions
+		column_position_list = []
 		for num in range(num_columns):
 			column_position = (num,self.board_position[0] + self.cell_width * num)
 			column_position_list.append(column_position)
 
 		# assign row positions
+		row_position_list = []
 		for num in range(num_rows):
 			row_position = (num,self.board_position[0] + self.cell_width * num)
 			row_position_list.append(row_position)
@@ -69,12 +65,44 @@ class Board():
 			]
 		
 		for cell_position in cell_position_list:
-			board_cell = Board.BoardCell(size=cell_width
+			board_cell = Board.BoardCell(
+				size=cell_width
 				,position=cell_position.position
 				,coordinate=cell_position.coordinate
 				)
 			self.board_cell_group.add(board_cell)
 
+		board_column = Board.BoardColumn(self.board_cell_group,0)		
+
+	class BoardCell(pygame.sprite.Sprite):
+		def __init__(self, size, position, coordinate):
+			super(Board.BoardCell,self).__init__()
+			self.surf = pygame.Surface((size,size))
+			self.rect = self.surf.get_rect()
+			self.rect = pygame.draw.rect(self.surf, color=(255,0,0), rect=self.rect, width=2)
+			
+			self.size = size
+			self.coordinate = coordinate
+			self.position = position
+			self.rect.left = position[0]
+			self.rect.top = position[1]
+	
+	class BoardColumn():
+		'''
+		TODO: 2023_07_29_3 - Find Open Board Cell
+		'''
+		def __init__(self,board_cell_group, column_index):
+			self.member_board_cell_list = [
+				board_cell for board_cell 
+				in board_cell_group 
+				if board_cell.coordinate[0] == column_index
+				]
+			
+			column_center_position = \
+				self.member_board_cell_list[0].position[0] \
+				+ self.member_board_cell_list[0].size*1.0/2
+
+			print(column_center_position)
 
 # create token object 
 token = Token()
@@ -85,6 +113,13 @@ token.rect.right = 50
 board = Board(board_size=(7,6),board_position=(100,100), cell_width=75)
 		
 def run_game():
+	# Initialize Pygame
+	pygame.init()
+
+	# Set up the display
+	width, height = 800, 600
+	screen = pygame.display.set_mode((width, height))
+
 	# mouse state
 	mouse_holding_token = False
 	mouse_button_down = False
@@ -136,5 +171,12 @@ def run_game():
 	pygame.quit()
 
 if __name__ == '__main__':
-	run_game()
+	# run_game()
+	# token = Token()
+	# token.rect.top = 50
+	# token.rect.right = 50
+	
 	# board = Board(board_size=(7,6),board_position=(200,200), cell_width=20)
+	# token.find_open_board_cell(board)
+	# board.get_board_column_positions()
+	pass
