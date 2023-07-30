3bf3140bbbb42c3f4214c0e5d27affc12408fdfd
Ben Shippey <shippeyben@gmail.com>
2023-07-30 19:18:28 -0400

    Created Player Turn Functionality
    Interface now exhibits base 'Connect Four' Functionality

--- program.py
+++ program.py
@@ -9,17 +9,36 @@ import copy
 Game Object Definitions
 '''
 class Token(pygame.sprite.Sprite):
-  def __init__(self, position):
+  def __init__(self, size, color, player):
     super(Token,self).__init__()
-    self.surf = pygame.Surface((position[0],position[1]))
-    self.surf.fill((255,0,0))
+    self.surf = pygame.Surface((size[0],size[1]))
+    self.surf.fill(color)
     self.surf.set_colorkey((255,255,255),RLEACCEL)
     self.rect = self.surf.get_rect()
+
+    self._player = player
     self._is_moveable = True
 
   @property
   def centerx(self):
      return self.rect.centerx
+  
+  @property
+  def position(self):
+     return self.rect.position
+  
+  @position.setter
+  def position(self,value):
+    self.rect.left = value[0]
+    self.rect.bottom = value[1]
+
+  @property
+  def player(self):
+     return self._player.name
+  
+  @player.setter
+  def player(self, value):
+     self._player = value
   
 class Board():
   '''
@@ -50,7 +69,7 @@ class Board():
     # assign row positions
     row_position_list = []
     for num in range(num_rows):
-      row_position = (num,self.board_position[0] + self.cell_width * num)
+      row_position = (num,self.board_position[1] + self.cell_width * num)
       row_position_list.append(row_position)
 
     # get cartesian product of column and row positions
@@ -77,7 +96,12 @@ class Board():
       board_column = Board.BoardColumn(self.board_cell_group,num)
       self.board_column_list.append(board_column)
 
-    # print(self.board_column_list)
+  @property
+  def bottom(self):
+     for cell in self.board_cell_group:
+        if cell.coordinate[0] == self.board_size[1]:
+           print(cell.rect.bottom)
+           return cell.rect.bottom
 
   class BoardCell(pygame.sprite.Sprite):
     def __init__(self, size, position, coordinate):
@@ -85,8 +109,8 @@ class Board():
       self.surf = pygame.Surface((size,size))
       self.rect = self.surf.get_rect()
       self.rect = pygame.draw.rect(self.surf, color=(255,0,0), rect=self.rect, width=2)
-      self.rect.top = position[1]
       self.rect.left = position[0]
+      self.rect.top = position[1]
       
       self._token = None
       self._size = size
@@ -156,6 +180,8 @@ class Board():
     '''
     coordinate_delta_list = [(1,0),(0,1),(1,1),(-1,1)]
 
+    current_player = board_cell.token.player
+
     for coordinate_delta in coordinate_delta_list: # for all directions
       delta_x = coordinate_delta[0]
       delta_y = coordinate_delta[1]
@@ -166,6 +192,11 @@ class Board():
       positive_direction = (delta_x,delta_y)
       negative_direction = (-delta_x,-delta_y)
       for direction in [positive_direction,negative_direction]:
+        # print(direction)
+        '''
+        This inner loop traverses the populated board cells
+        
+        '''
         direction_x = direction[0]
         direction_y = direction[1]
 
@@ -173,17 +204,28 @@ class Board():
         current_y = board_cell.coordinate[1] + direction_y
 
         current_cell = self.get_board_cell(current_x,current_y)
+
+        def get_seek_status(current_cell):
+          if current_cell is None or current_cell.token is None or current_cell.token.player != current_player: 
+            return False
+          else:
+            return True
         
-        while current_cell is not None:
+        is_seeking = get_seek_status(current_cell)
+        while is_seeking: 
+          # record status
+          contiguous_cell_counter += 1
+          contiguous_cell_list.append(current_cell)
+      
+          # advance to next cell
           current_x += direction_x
           current_y += direction_y
-          if current_cell.token is not None: 
-            contiguous_cell_counter += 1
-            contiguous_cell_list.append(current_cell)
           current_cell = self.get_board_cell(current_x,current_y)
+          is_seeking = get_seek_status(current_cell)
 
       if len(contiguous_cell_list) > 1: 
         print(contiguous_cell_list)
+        # pass
       
       if len(contiguous_cell_list) == contiguity_limit:
           return 'CONTIGUITY_LIMIT_REACED'
@@ -227,24 +269,66 @@ def move_sprite_to_sprite(sprite_source,sprite_target):
    sprite_source.rect.left = sprite_target.rect.left
    sprite_source.rect.top = sprite_target.rect.top
 
+'''
+Game Management
+'''
+
+class GameManager():
+  def __init__(self):
+     self._current_turn = 0
+     self._first_turn = True
+     self._count_players = 2
+     self._turns_per_round = self._count_players
+     self._players = [Player('Player 1'), Player('Player 2')]
+  
+  @property
+  def players(self):
+     return self._players 
+  
+  def next_turn(self):
+      if self._first_turn:
+        self._first_turn = False
+        self._current_turn = 0
+      else:
+        self._current_turn = (self._current_turn + 1) % self._turns_per_round # wraps back to starting player if exceeds count of players
+
+      if self._current_turn == 0:
+          token = Token((75,75),(255,0,0), self.players[0])
+      elif self._current_turn == 1:
+          token = Token((75,75),(0,255,0),self.players[1])
+      return token
+   
+
+class Player():
+
+  def __init__(self,name):
+    self.name = name
+
 def run_game():
+  '''
+  Game Runtime
+  '''
   # Initialize Pygame
   pygame.init()
 
   # Set up the display
-  width, height = 800, 600
+  width, height = 1200, 800
   screen = pygame.display.set_mode((width, height))
+
+  # create grid_square objects
+  board = Board(board_size=(7,6),board_position=(300,200), cell_width=75)
   
-  # create token object 
-  active_token = Token((75,75))
+  # initialize game and sprite managers
+  game_manager = GameManager()
   token_group = pygame.sprite.Group()
+
+  # create token object 
+  active_token = game_manager.next_turn()
   token_group.add(active_token)
+  active_token.position = (50, 400)
 
   # initialize other runtime objects
   target_board_cell = None
-
-  # create grid_square objects
-  board = Board(board_size=(7,6),board_position=(100,100), cell_width=75)
       
   # mouse state
   mouse_holding_token = False
@@ -256,9 +340,6 @@ def run_game():
   while running:
       # Handle events
       for event in pygame.event.get():
-          # set persistent states
-          # is token held by user
-
           if event.type == MOUSEBUTTONDOWN:
             mouse_button_down = True
             mouse_token_collide = active_token.rect.collidepoint(event.pos)
@@ -275,8 +356,9 @@ def run_game():
               target_board_cell = board.find_board_cell(active_token)				
               target_board_cell.token = active_token
               move_sprite_to_sprite(target_board_cell.token,target_board_cell)
-              active_token = Token((75,75))
+              active_token = game_manager.next_turn()
               token_group.add(active_token)
+              active_token.position = (50,400)
             
             # evaluate game status
             if target_board_cell is not None:
@@ -284,13 +366,10 @@ def run_game():
               if game_status == 'CONTIGUITY_LIMIT_REACED':
                 running = False
 
-            # print([cell for cell in board.board_cell_group if cell.token])
           if mouse_holding_token:
               if event.type == MOUSEMOTION:
                   token.rect.move_ip(event.rel)
 
-          
-          
           # other event processing
           if event.type == pygame.QUIT:
               running = False
@@ -314,12 +393,4 @@ def run_game():
 
 if __name__ == '__main__':
   run_game()
-  # token_position = (50,50)
-  # token = Token(token_position)
-  
-  # board = Board(board_size=(7,6),board_position=(200,200), cell_width=20)
-  # print([cell for cell in board.board_cell_group if cell.token])
-  # board.find_board_cell(token)
-  # token.find_open_board_cell(board)
-  # board.get_board_column_positions()
-  pass
+  # board = Board(board_size=(7,6),board_position=(100,100), cell_width=75)git 
